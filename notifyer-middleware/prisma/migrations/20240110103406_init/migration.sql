7e3ecb83e6c421484a683c7dd9ca0313feab1f9c
Paulo Sérgio <paulosmdo@gmail.com>
2024-01-10 07:39:49 -0300

    nova migracao

---
+++ notifyer-middleware/prisma/migrations/20240110103406_init/migration.sql
@@ -0,0 +1,36 @@
+-- CreateTable
+CREATE TABLE "Users" (
+    "Id" TEXT NOT NULL,
+    "Name" TEXT NOT NULL,
+    "Email" TEXT NOT NULL,
+    "Password" VARCHAR(255) NOT NULL,
+    "CreatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "UpdatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Users_pkey" PRIMARY KEY ("Id")
+);
+
+-- CreateTable
+CREATE TABLE "Tokens" (
+    "Id" TEXT NOT NULL,
+    "UserId" TEXT NOT NULL,
+    "AccessToken" TEXT NOT NULL,
+    "SessionKey" TEXT NOT NULL,
+
+    CONSTRAINT "Tokens_pkey" PRIMARY KEY ("Id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_Email_key" ON "Users"("Email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Tokens_UserId_key" ON "Tokens"("UserId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Tokens_AccessToken_key" ON "Tokens"("AccessToken");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Tokens_SessionKey_key" ON "Tokens"("SessionKey");
+
+-- AddForeignKey
+ALTER TABLE "Tokens" ADD CONSTRAINT "Tokens_UserId_fkey" FOREIGN KEY ("UserId") REFERENCES "Users"("Id") ON DELETE RESTRICT ON UPDATE CASCADE;
